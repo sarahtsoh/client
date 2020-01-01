26d3263b2fbcf588eb5c3fe1567f57e57d675e53
sarah <sarah_tsoh@yahoo.com.au>
2020-01-01 22:54:34 +1100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using Grpc.Core;
+using Grpc.Net.Client;
+using Grpc_Demo2;
+using System;
+using System.Threading.Tasks;
+
+namespace client
+{
+    class Program
+    {
+        static async Task Main()
+        {
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new Greeter.GreeterClient(channel);
+
+            var response = await client.SayHelloAsync(
+                new HelloRequest { Name = "World" });
+            Console.WriteLine(response);
+
+
+            using var streamingCall = client.SayHelloStream(
+              new HelloRequest { Name = "World" });
+
+            await foreach (var reply in streamingCall.ResponseStream.ReadAllAsync())
+            {
+                Console.WriteLine(reply.Message);
+            }
+
+        }
+    }
+}
